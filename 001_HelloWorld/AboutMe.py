49731838b93f5e63824d5f1b9de31b17d59d29cb
Guibs <805249506@qq.com>
2016-09-03 01:47:29 +0800

    个人及项目介绍

---
+++ 001_HelloWorld/AboutMe.py
@@ -0,0 +1,7 @@
+# 个人及项目介绍
+print("---------------------")
+print("Author: Guibs[桂布斯]")
+print("Book: 《Python编程：从入门到实践》")
+print("GitHub: https://github.com/Guibs82")
+print("Python: 3.5.2")
+print("---------------------")
