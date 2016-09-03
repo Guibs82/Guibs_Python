da6aea5beddc0736b3882e49d8f4349bf662c2c4
Guibs <805249506@qq.com>
2016-09-03 19:59:19 +0800

    变量
    变量

---
+++ 002_Variable/variable.py
@@ -0,0 +1,13 @@
+# 变量的命名和使用
+
+# 变量名只能包含字母、数字和下划线. [不能以数字打头] [001_send message x][send_message_001 ✔]️
+# 变量名不能包含空格, 可以使用下划线来分割单词 [send message x][send_message ✔]️
+# 不能将 Python 关键字和函数名用作变量名
+# 变量名应简短且有描述性 例如姓名: [n x][name ✔]️
+# 注意 l 和 O 的使用, 部分字体中会与 1 和 0 混淆
+
+message = "Hello Guibs"
+print(message)
+
+message = "Fighting Guibs"
+print(message)
